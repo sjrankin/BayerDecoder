9ea0523461ba80b45e1a7ccd8371ac9d0d3d1916
Stuart Rankin <sjrankin@gmail.com>
2020-07-27 15:28:25 +0900

    Create BayerDecodeParameters.swift

---
+++ BayerDecoder/MetalKernels/BayerDecodeParameters.swift
@@ -0,0 +1,16 @@
+//
+//  BayerDecodeParameters.swift
+//  BayerDecoder
+//
+//  Created by Stuart Rankin on 7/27/20.
+//
+
+import Foundation
+import simd
+
+struct BayerDecodeParameters
+{
+    let Order: simd_uint1
+    let Method: simd_uint1
+}
+
