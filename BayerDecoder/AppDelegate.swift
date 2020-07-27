65a81ad95d08a9c96c76e5a42c6a1b516f944b1a
Stuart Rankin <sjrankin@gmail.com>
2020-07-27 10:03:43 +0900

    Initial Commit

---
+++ BayerDecoder/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  BayerDecoder
+//
+//  Created by Stuart Rankin on 7/27/20.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ BayerDecoder/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  BayerDecoder
+//
+//  Created by Stuart Rankin on 7/27/20.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
